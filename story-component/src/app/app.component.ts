78403979d0b932a20243036972591059b408ed9b
Gaurav bhojwani <www.gaurav10bhojwani@gmail.com>
2024-07-28 18:39:34 +0000

    multi person story in progress

--- story-component/src/app/app.component.ts
+++ story-component/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, Injectable } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SwipeDirective } from './swipe.directive';
 import { HAMMER_GESTURE_CONFIG, HammerGestureConfig, HammerModule } from '@angular/platform-browser';
 declare var Hammer: any;
 
@@ -23,12 +22,28 @@ export class MyHammerConfig extends HammerGestureConfig {
 })
 export class AppComponent {
   title = 'story-component';
-  stories = [
-    { id: 1, type: 'image', content: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/1.jpg' },
-    { id: 2, type: 'image', content: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/2.jpg' },
-    { id: 3, type: 'image', content: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/3.jpg' }
+  readonly persons = [
+    {
+      id: 1,
+      name: 'Gaurav',
+      stories: [
+        { id: 1, type: 'image', content: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/1.jpg' },
+        { id: 2, type: 'image', content: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/2.jpg' },
+        { id: 3, type: 'image', content: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/3.jpg' }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Rajesh',
+      stories: [
+        { id: 1, type: 'image', content: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/4.jpg' },
+        { id: 2, type: 'image', content: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/5.jpg' },
+        { id: 3, type: 'image', content: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/6.jpg' }
+      ]
+    },
   ];
   currentStoryIndex = 0;
+  currentPersonIndex = 0;
   progressWidth = 0;
   intervalId: any;
   isTransitioning = false;
@@ -54,7 +69,8 @@ export class AppComponent {
     if (this.isTransitioning) return;
     this.isTransitioning = true;
     clearInterval(this.intervalId);
-    this.currentStoryIndex = (this.currentStoryIndex + 1) % this.stories.length;
+    const stories = this.persons.find((person,index)=>)
+    this.currentStoryIndex = (this.currentStoryIndex + 1) % this.persons.length;
     this.progressWidth = 0;
     setTimeout(() => {
       this.startStoryProgress();
@@ -63,14 +79,40 @@ export class AppComponent {
   }
 
   prevStory() {
+
+  }
+
+  nextPersonStory() {
     if (this.isTransitioning) return;
     this.isTransitioning = true;
+    this.currentStoryIndex=0;
     clearInterval(this.intervalId);
-    this.currentStoryIndex = (this.currentStoryIndex - 1 + this.stories.length) % this.stories.length;
+    this.currentPersonIndex = (this.currentPersonIndex + 1) % this.persons.length;
     this.progressWidth = 0;
     setTimeout(() => {
       this.startStoryProgress();
       this.isTransitioning = false;
     }, 500); // Match this timeout with the CSS transition duration
+  }
+
+  prevPersonStory() {
+    if (this.isTransitioning) return;
+    this.isTransitioning = true;
+    this.currentStoryIndex=0;
+    clearInterval(this.intervalId);
+    this.currentPersonIndex = (this.currentPersonIndex - 1 + this.persons.length) % this.persons.length;
+    this.progressWidth = 0;
+    setTimeout(() => {
+      this.startStoryProgress();
+      this.isTransitioning = false;
+    }, 500); // Match this timeout with the CSS transition duration
+  }
+
+  onPreviousTap() {
+
+  }
+
+  onNextTap() {
+
   }
 }
